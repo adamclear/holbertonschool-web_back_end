d757017f194884e266c4923757f5a441d542668d
adamclear <3589@holbertonschool.com>
2022-05-04 18:00:27 +0000

    eslint fix

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,10 +4,10 @@ import uploadPhoto from './5-photo-reject';
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   return [
     await signUpUser(firstName, lastName)
+      /* signUpUser only has a resolve no need for a catch */
       .then((response) => ({ status: 'fulfilled', value: response })),
-      /* signUpUser only has a resolve no need for a catch*/
     await uploadPhoto(fileName)
-      /* uploadPhoto only has a reject no need for a then*/
+      /* uploadPhoto only has a reject no need for a then */
       .catch((response) => ({ status: 'rejected', value: response.toString() })),
   ];
 }
