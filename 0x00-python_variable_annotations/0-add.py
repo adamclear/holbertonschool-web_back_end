2cc867e607bf9d76f4d1d403f3027c8e50dcaa2c
adamclear <3589@holbertonschool.com>
2022-08-29 17:51:09 +0000

    added blank line for pycodestyle to be happy

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -2,6 +2,7 @@
 ''' A function that adds two floats and returns their sum as a float. This
 function is to be type-annotated '''
 
+
 def add(a: float, b: float) -> float:
     ''' Sums a + b and returns the result '''
     return a + b
