7a777016a5ea7c97f68aead0af57d99093d9c876
adamclear <3589@holbertonschool.com>
2022-08-29 17:58:06 +0000

    file 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+''' This function returns the floor of a given float. This function is to
+be type-annotated. '''
+
+
+def floor(n: float) -> float:
+    ''' Returns the floor of the float '''
+    return floor(n)
