76e6b5555aad1fae93bd3d500c965b9239b5f089
adamclear <3589@holbertonschool.com>
2022-08-29 18:12:04 +0000

    updated

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-import math
 ''' This function returns the floor of a given float. This function is to
 be type-annotated. '''
 
 
 def floor(n: float) -> int:
     ''' Returns the floor of the float '''
+    import math
     return math.floor(n)
