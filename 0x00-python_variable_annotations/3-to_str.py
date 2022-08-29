3c63ecda043a15a65d1b5e47c33e40df6ac6f998
adamclear <3589@holbertonschool.com>
2022-08-29 18:14:44 +0000

    file 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+''' This function takes a float and returns the string representation of
+that float. This function is to be type-annotated. '''
+
+
+def to_str(n: float) -> str:
+    ''' Returns float cast to str '''
+    return str(n)
