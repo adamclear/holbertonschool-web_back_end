abf86d51553b3e4dde4638c7abcd03cafc74e499
adamclear <3589@holbertonschool.com>
2022-08-29 17:50:12 +0000

    added directory, file 0, README

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+''' A function that adds two floats and returns their sum as a float. This
+function is to be type-annotated '''
+
+def add(a: float, b: float) -> float:
+    ''' Sums a + b and returns the result '''
+    return a + b
---
+++ 0x00-python_variable_annotations/README.md
@@ -0,0 +1,1 @@
+This lesson is all about Python Variable Annotations
