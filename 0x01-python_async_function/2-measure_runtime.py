84f91d0105f1643d2c7239220980466aadde01e4
adamclear <3589@holbertonschool.com>
2022-08-30 18:56:58 +0000

    updated

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -4,13 +4,13 @@ the given n number of times to run wait_random and the given max_delay. '''
 
 import asyncio
 import time
-wait_n = __import__('1-concurrent-coroutines').wait_n
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
     ''' Uses asyncio to run wait_n using the given variables and uses
     time to measure the amount of time it takes to run. '''
-    startTime: float = time.time()
+    startTime = time.time()
     asyncio.run(wait_n(n, max_delay))
-    endTime: float = time.time()
+    endTime = time.time()
     return (endTime - startTime) / n
