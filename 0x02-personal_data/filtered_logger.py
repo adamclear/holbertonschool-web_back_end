10b96bab990a282194c1a5d4cd74a50c69adfb95
adamclear <3589@holbertonschool.com>
2022-09-13 20:31:24 +0000

    updated for tasks 1 & 2

--- 0x02-personal_data/filtered_logger.py
+++ 0x02-personal_data/filtered_logger.py
@@ -1,9 +1,33 @@
 #!/usr/bin/env python3
 ''' This module contains the class RedactingFormatter and other functions
 related to its use. '''
+import logging
 import re
 from typing import List
 
+
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        ''' Class initialization. '''
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = list(fields)
+
+    def format(self, record: logging.LogRecord) -> str:
+        ''' Formats the log record. '''
+        return filter_datum(self.fields,
+                            self.REDACTION,
+                            super().format(record),
+                            self.SEPARATOR)
 
 def filter_datum(fields: List[str],
                  redaction: str,
@@ -15,3 +39,13 @@ def filter_datum(fields: List[str],
         redaction,
         message
     )
+
+def get_logger() -> logging.Logger:
+    ''' Creates and defines a logger. '''
+    user_data = logging.getLogger('user_data')
+    user_data.setLevel(logging.INFO)
+    user_data.propagate = False
+    streamHandler = logging.StreamHandler()
+    streamHandler.setFormatter(RedactingFormatter(PII_FIELDS))
+    user_data.addHandler(streamHandler)
+    return user_data
