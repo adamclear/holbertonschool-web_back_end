b0be126136c32c102de97b2472232586cced4172
adamclear <3589@holbertonschool.com>
2022-09-13 20:01:58 +0000

    added directory, README, file 0

---
+++ 0x02-personal_data/filtered_logger.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+''' This module contains the class RedactingFormatter and other functions
+related to its use. '''
+import re
+from typing import List
+
+
+def filter_datum(
+    fields: List[str],
+    redaction: str,
+    message: str,
+    separator: str) -> str:
+    ''' Returns a log message with sensitive data obfuscated. '''
+    return re.sub(
+        '|'.join(f"(?<={field}=).*?(?={separator})" for field in fields),
+        redaction,
+        message
+    )
