fd4a412b7d8082a6cbf58437e680784573a4a77c
adamclear <3589@holbertonschool.com>
2022-09-19 22:48:25 +0000

    task 9

--- 0x03-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x03-Basic_authentication/api/v1/auth/basic_auth.py
@@ -25,3 +25,14 @@ class BasicAuth(Auth):
             return decode_base.decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        ''' Returns the user email and password from the
+        Base64 decoded value '''
+        if decoded_base64_authorization_header is None or\
+                type(decoded_base64_authorization_header) != str or\
+                ':' not in decoded_base64_authorization_header:
+            return (None, None)
+        return (decoded_base64_authorization_header.split(':')[0],
+                decoded_base64_authorization_header.split(':')[1])
