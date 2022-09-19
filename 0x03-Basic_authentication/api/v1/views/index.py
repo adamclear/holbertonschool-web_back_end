417134ef4c216d7b96f2a3e5062c4b6b626d237b
adamclear <3589@holbertonschool.com>
2022-09-19 21:25:43 +0000

    updated

--- 0x03-Basic_authentication/api/v1/views/index.py
+++ 0x03-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,8 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    ''' For testing the unauthorized 401 handler '''
+    abort(401)
