3d118409399c910363af312a2121d70a01cdda99
adamclear <3589@holbertonschool.com>
2022-09-19 21:51:49 +0000

    task 4

--- 0x03-Basic_authentication/api/v1/auth/auth.py
+++ 0x03-Basic_authentication/api/v1/auth/auth.py
@@ -7,8 +7,14 @@ from typing import List, TypeVar
 class Auth():
     ''' API authentication class '''
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        ''' Currently just returns False '''
-        return False
+        ''' Defines which routes don't need authentication. '''
+        if path is None or excluded_paths is None:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        if path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         ''' Currently just returns None '''
