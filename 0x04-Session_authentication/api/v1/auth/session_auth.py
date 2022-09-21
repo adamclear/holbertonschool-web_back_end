0b225d83023c37e9ef7889f2c83c7c7ee9926b81
adamclear <3589@holbertonschool.com>
2022-09-21 19:47:32 +0000

    task 2

--- 0x04-Session_authentication/api/v1/auth/session_auth.py
+++ 0x04-Session_authentication/api/v1/auth/session_auth.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 ''' SessionAuth class '''
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     ''' SessionAuth class '''
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        ''' Creates a session ID for a user_id '''
+        if user_id is None or type(user_id) != str:
+            return None
+        session_id = str(uuid4())
+        self.user_id_by_session_id.update({session_id: user_id})
+        return session_id
