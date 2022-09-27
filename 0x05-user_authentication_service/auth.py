9ce53bcd2eb1fa02da1e10409ba8c0fc93552f37
adamclear <3589@holbertonschool.com>
2022-09-27 22:12:45 +0000

    task 8

--- 0x05-user_authentication_service/auth.py
+++ 0x05-user_authentication_service/auth.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 ''' Auth functions '''
+from xmlrpc.client import Boolean
 import bcrypt
 from db import DB
 from sqlalchemy.orm.exc import NoResultFound
@@ -25,3 +26,12 @@ class Auth:
             raise ValueError('User {} already exists'.format(email))
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
+
+    def valid_login(self, email: str, password: str) -> Boolean:
+        ''' Finds User and checks password '''
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('utf-8'),
+                                  user.hashed_password)
+        except NoResultFound:
+            return False
