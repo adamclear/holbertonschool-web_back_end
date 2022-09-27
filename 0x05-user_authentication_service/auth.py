d8cad25e4c15d2c1928791df4e7430593731ca91
adamclear <3589@holbertonschool.com>
2022-09-27 22:49:36 +0000

    task 12

--- 0x05-user_authentication_service/auth.py
+++ 0x05-user_authentication_service/auth.py
@@ -51,3 +51,10 @@ class Auth:
             return session_id
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> str:
+        ''' Returns a user based on session id '''
+        try:
+            return self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
