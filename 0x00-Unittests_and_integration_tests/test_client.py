86e676e121ac262e0cbb668dade9257c0742ee0a
adamclear <3589@holbertonschool.com>
2022-10-03 20:33:35 +0000

    updated for task 6

--- 0x00-Unittests_and_integration_tests/test_client.py
+++ 0x00-Unittests_and_integration_tests/test_client.py
@@ -29,3 +29,15 @@ class TestGithubOrgClient(TestCase):
                    return_value={"repos_url": "url"}):
             client = GithubOrgClient("google")
             self.assertEqual(client._public_repos_url, "url")
+
+    @patch("client.get_json")
+    def test_public_repos(self, mockJson):
+        ''' public_repos tests '''
+        with patch("client.GithubOrgClient.public_repos",
+                   new_callable=PropertyMock) as mockRepo:
+            goc = GithubOrgClient("org_name")
+            mockJson.return_value = {"repos_url": "url"}
+            mockRepo.return_value = goc.org.get("repos_url")
+            self.assertEqual(goc.public_repos, "url")
+            mockJson.assert_called_once()
+            mockRepo.assert_called_once()
