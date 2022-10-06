bd790e11d1347e2a5dc838e3b0c0b75e2a82efc0
adamclear <3589@holbertonschool.com>
2022-10-06 19:31:01 +0000

    corrected to display correct index pages

--- 0x01-i18n/4-app.py
+++ 0x01-i18n/4-app.py
@@ -32,7 +32,7 @@ def get_locale():
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index():
     ''' Index page '''
-    return render_template('2-index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == '__main__':
