0fe5a7c7f4fbc7fb0b0697c87bdc2fc3968a1f75
adamclear <3589@holbertonschool.com>
2022-10-06 19:12:30 +0000

    files for task 2

--- 0x01-i18n/2-app.py
+++ 0x01-i18n/2-app.py
@@ -2,7 +2,7 @@
 ''' Flask app '''
 
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
@@ -20,10 +20,16 @@ class Config(object):
 app.config.from_object(Config)
 
 
+@babel.localeselector
+def get_locale():
+    ''' Locale selector '''
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index():
     ''' Index page '''
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == '__main__':
