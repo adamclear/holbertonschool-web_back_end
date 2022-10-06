7b14d117fee991fa332385d4b0e207f2bb36e7cb
adamclear <3589@holbertonschool.com>
2022-10-06 20:27:09 +0000

    files for task 7, made all app files executable

--- 0x01-i18n/7-app.py
+++ 0x01-i18n/7-app.py
@@ -78,8 +78,8 @@ def get_locale():
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index():
     ''' Index page '''
-    return render_template('6-index.html')
+    return render_template('7-index.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True)
