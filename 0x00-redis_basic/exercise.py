078e31203c99d14f74f74e1f9ef07370a9b747c6
adamclear <3589@holbertonschool.com>
2022-10-10 18:46:26 +0000

    task 1

--- 0x00-redis_basic/exercise.py
+++ 0x00-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 
 
 import redis
-from typing import Union
+from typing import Callable, Union
 import uuid
 
 
@@ -19,3 +19,17 @@ class Cache():
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, int]:
+        ''' Retrieve data from cache and convert back to type '''
+        if fn:
+            return fn(self._redis.get(key))
+        return self._redis.get(key)
+
+    def get_str(self, key: str) -> str:
+        ''' Gets str type data from cache '''
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> int:
+        ''' Gets int type data from cache '''
+        return self.get(key, int)
