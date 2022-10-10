903cd27232372db56de241fc4cf8218749271590
adamclear <3589@holbertonschool.com>
2022-10-10 19:04:57 +0000

    task 3

--- 0x00-redis_basic/exercise.py
+++ 0x00-redis_basic/exercise.py
@@ -22,6 +22,19 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    ''' Stores method call history '''
+    @wraps(method)
+    def wrapper(self, *args) -> Union[int, str]:
+        ''' History wrapper '''
+        key = method.__qualname__
+        self._redis.rpush(f"{key}:inputs", str(args))
+        self._redis.rpush(f"{key}:outputs", method(self, *args))
+        return method(self, *args)
+
+    return wrapper
+
+
 class Cache():
     ''' Caching class '''
     def __init__(self):
@@ -29,6 +42,7 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         ''' Data storage '''
