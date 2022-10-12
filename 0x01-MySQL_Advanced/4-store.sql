f0131423b8a6bc90be2a91ffc302af15602e922c
adamclear <3589@holbertonschool.com>
2022-10-12 18:45:50 +0000

    updated

--- 0x01-MySQL_Advanced/4-store.sql
+++ 0x01-MySQL_Advanced/4-store.sql
@@ -3,6 +3,6 @@
 CREATE TRIGGER sold
 AFTER INSERT ON orders
 FOR EACH ROW
-UPDATE items
-SET quantity = quantity - NEW.number
-WHERE name = NEW.item_name;
+	UPDATE items
+	SET quantity = quantity - NEW.number
+	WHERE name = NEW.item_name;
