ce3aceeb29361885663db23d3555d28a4b843e97
adamclear <3589@holbertonschool.com>
2022-10-22 19:46:40 +0000

    updated

--- 0x00-Node_JS_basic/1-stdin.js
+++ 0x00-Node_JS_basic/1-stdin.js
@@ -3,12 +3,12 @@
 
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin
-	.on('readable', () => {
-		const name = process.stdin.read();
-		if (name) {
-			process.stdout.write(`Your name is: ${name}`);
-		}
-	})
-	.on('end', () => {
-		process.stdout.write('This important software is now closing\n');
-	});
+  .on('readable', () => {
+    const name = process.stdin.read();
+    if (name) {
+      process.stdout.write(`Your name is: ${name}`);
+    }
+  })
+  .on('end', () => {
+    process.stdout.write('This important software is now closing');
+  });
