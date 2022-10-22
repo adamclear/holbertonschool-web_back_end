d52c7ca606c32d1c9becd539604b17ce15203941
adamclear <3589@holbertonschool.com>
2022-10-22 19:18:14 +0000

    updated

--- 0x02-NoSQL/12-log_stats.py
+++ 0x02-NoSQL/12-log_stats.py
@@ -6,13 +6,14 @@ from pymongo import MongoClient
 
 def log_stats():
     ''' Provides stats about Nginx logs stored in MongoDB '''
-    logs = MongoClient().logs.nginx
-    print(f"{logs.count_documents({})} logs")
+    mongoLog = MongoClient().logs.nginx
+    print(f"{mongoLog.count_documents({})} logs")
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
-        print(f"\tmethod {method}: {logs.count_documents({'method': method})}")
-    print(f"{logs.count_documents({'method': 'GET', 'path': '/status'})}\
+        print(f"\tmethod {method}: \
+            {mongoLog.count_documents({'method': method})}")
+    print(f"{mongoLog.count_documents({'method': 'GET', 'path': '/status'})}\
         status check")
 
 
