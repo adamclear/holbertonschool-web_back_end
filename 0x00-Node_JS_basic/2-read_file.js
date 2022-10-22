7a4028d4da5f2d317cf203f82e8ac13c99596180
adamclear <3589@holbertonschool.com>
2022-10-22 20:42:55 +0000

    file 2

---
+++ 0x00-Node_JS_basic/2-read_file.js
@@ -0,0 +1,31 @@
+// Logs # of students in each field of database file
+
+const fs = require('fs');
+
+const countStudents = (path) => {
+  let students
+  try {
+    students = fs.readFileSync(path, 'utf-8');
+  } catch (error) {
+    throw Error('Cannot load the database');
+  }
+  const lines = students.split('\n').slice(1).filter((line) =>
+  line !== '');
+  console.log(`Number of students: ${lines.length}`);
+  const fields = [...new Set(lines.map((line) => line.split(',')[3]))];
+  fields.forEach((field) => {
+    const studentsInField = lines
+      .filter((line) => line.endsWith(field))
+      .map((line) => {
+        const firstName = line.split(',');
+        return firstName[0];
+      });
+    console.log(
+      `Number of students in ${field}: ${
+        studentsInField.length
+      }. List: ${studentsInField.join(', ')}`
+    );
+  });
+};
+
+module.exports = countStudents;
