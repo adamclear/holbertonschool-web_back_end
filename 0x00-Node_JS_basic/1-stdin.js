652347f35b3ca54d44cffa1421cc3bdfd77df6f0
adamclear <3589@holbertonschool.com>
2022-10-22 19:43:09 +0000

    1-stdin.js

---
+++ 0x00-Node_JS_basic/1-stdin.js
@@ -0,0 +1,14 @@
+// Displays welcome message and asks for name, then displays users
+// input name
+
+console.log("Welcome to Holberton School, what is your name?");
+process.stdin
+	.on('readable', () => {
+		const name = process.stdin.read();
+		if (name) {
+			process.stdout.write(`Your name is: ${name}`);
+		}
+	})
+	.on('end', () => {
+		process.stdout.writableFinished("This important software is now closing\n");
+	});
