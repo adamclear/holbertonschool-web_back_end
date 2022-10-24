012acd81242757207ccbd21a0ffa64351b4917da
adamclear <3589@holbertonschool.com>
2022-10-24 00:50:26 +0000

    updated

--- 0x00-Node_JS_basic/4-http.js
+++ 0x00-Node_JS_basic/4-http.js
@@ -1,8 +1,7 @@
 // Creates a small HTTP server using the http module
 const http = require('http');
 
-const app = http
-  .createServer((request, result) => {
+const app = http.createServer((request, result) => {
     result.end('Hello Holberton School!');
   })
   .listen(1245);
