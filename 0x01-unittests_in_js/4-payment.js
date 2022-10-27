921956da82811bc95b530bebe82b096bd6888036
adamclear <3589@holbertonschool.com>
2022-10-27 19:03:31 +0000

    files for task 4

---
+++ 0x01-unittests_in_js/4-payment.js
@@ -0,0 +1,9 @@
+// Function that takes two arguments and uses calculateNumber
+const Utils = require('./utils.js')
+
+const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
+	const total = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+	console.log(`The total is: ${total}`);
+}
+
+module.exports = sendPaymentRequestToApi;
---
+++ 0x01-unittests_in_js/4-payment.test.js
@@ -0,0 +1,17 @@
+// Tests for 3-payment.js
+const expect = require('chai').expect;
+const sendPaymentRequestToApi = require('./3-payment.js');
+const sinon = require('sinon');
+const Utils = require('./utils.js');
+
+describe('sendPaymentRequestToApi', () => {
+	it('Ensure that sendPayment... is using calculateNumber', () => {
+		const spyConsole = sinon.spy(console, 'log');
+		const stubUtils = sinon.stub(Utils, 'calculateNumber').returns(10);
+		sendPaymentRequestToApi(100, 20);
+		expect(stubUtils.calledWith('SUM', 100, 20)).to.be.true;
+		expect(spyConsole.calledWith('The total is: 10')).to.be.true;
+		spyConsole.restore();
+		stubUtils.restore();
+	});
+});
